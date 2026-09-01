9a36eedf1f632fc8697cb362a62266f249023868
Atif Sarfraz <atifsarfraz80@gmail.com>
2026-09-02 00:23:13 +0500

    Modified pool.js for deployment.

--- db/pool.js
+++ db/pool.js
@@ -5,10 +5,15 @@ dotenv.config();
 
 const { Pool } = pg;
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const pool = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: Number(process.env.DB_PORT) || 5432,
+  connectionString: process.env.DATABASE_URL || {
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: Number(process.env.DB_PORT) || 5432,
+  },
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
